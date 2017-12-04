afc1aee3c82f9a212c33ee4a99e8b03ad760a9d2
Nikolay Chernov <nchernov@gmail.com>
2017-12-04 22:09:54 +0300

    map3

--- server/game_config.py
+++ server/game_config.py
@@ -11,20 +11,20 @@ PARASITES_POWER_RANGE = (1, 3)
 TOWN_LEVELS = AttrDict({
     1: {
         'population_capacity': 10,
-        'product_capacity': 10,
-        'armor_capacity': 10,
-        'next_level_price': 10,
+        'product_capacity': 1000,
+        'armor_capacity': 20,
+        'next_level_price': 100,
     },
     2: {
         'population_capacity': 20,
-        'product_capacity': 20,
-        'armor_capacity': 20,
-        'next_level_price': 20,
+        'product_capacity': 2000,
+        'armor_capacity': 100,
+        'next_level_price': 500,
     },
     3: {
         'population_capacity': 40,
-        'product_capacity': 40,
-        'armor_capacity': 40,
+        'product_capacity': 10000,
+        'armor_capacity': 500,
         'next_level_price': None,
     },
 })
@@ -34,16 +34,16 @@ TRAIN_LEVELS = AttrDict({
         'goods_capacity': 100,
         # 'fuel_capacity': 100,
         # 'fuel_consumption': 1,
-        'next_level_price': 5,
+        'next_level_price': 50,
     },
     2: {
         'goods_capacity': 200,
         # 'fuel_capacity': 200,
         # 'fuel_consumption': 1,
-        'next_level_price': 10,
+        'next_level_price': 200,
     },
     3: {
-        'goods_capacity': 300,
+        'goods_capacity': 500,
         # 'fuel_capacity': 300,
         # 'fuel_consumption': 1,
         'next_level_price': None,
